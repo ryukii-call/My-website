9249970a1d338bd1a51511f9e5499bb950aba6a7
ryukii-call <ryu376237@gmail.com>
2026-05-08 11:19:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,72 @@
   window.onload = function () {
 
   const container = document.getElementById("container");
-  const form = document.getElementById("loginForm");
-  const error = document.getElementById("error");
 
   // ===== SCROLL AUTO KE LOGIN =====
   let sudahScroll = false;
 
   container.addEventListener("scroll", () => {
+
     if (!sudahScroll && container.scrollTop > 50) {
+
       sudahScroll = true;
 
       container.scrollTo({
         top: window.innerHeight,
         behavior: "smooth"
       });
+
     }
+
   });
 
   // ===== LOGIN =====
-  form.addEventListener("submit", function(e) {
+  const form = document.getElementById("loginForm");
+  const error = document.getElementById("error");
+
+  form.addEventListener("submit", function(e){
+
     e.preventDefault();
 
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+    const username =
+      document.getElementById("username").value;
 
-    console.log(username, password); // debug
+    const password =
+      document.getElementById("password").value;
 
-    if (username === "admin" && password === "1234") {
-      alert("Login berhasil!");
+    // ADMIN
+    if(
+      username === "admin" &&
+      password === "admin123"
+    ){
+
+      localStorage.setItem("role", "admin");
+      localStorage.setItem("username", "ADMIN 👑");
 
       window.location.href = "home.html";
 
-    } else {
-      error.textContent = "DASAR ANAK TOLOL!";
     }
+
+    // USER
+    else if(
+      username === "user" &&
+      password === "user123"
+    ){
+
+      localStorage.setItem("role", "user");
+      localStorage.setItem("username", "USER");
+
+      window.location.href = "home.html";
+
+    }
+
+    else{
+
+      error.innerHTML =
+      "Username atau password salah!";
+
+    }
+
   });
 
 };
